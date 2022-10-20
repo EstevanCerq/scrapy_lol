00565bf07a1876d61830424753ce0f6bbbb9c676
HugoWinterGhost <hfief1806@gmail.com>
2022-10-20 12:07:46 +0200

    add spider lol

--- WebCrawler/WebCrawler/spiders/scrapy_lol.py
+++ WebCrawler/WebCrawler/spiders/scrapy_lol.py
@@ -1,10 +1,28 @@
 import scrapy
+from scrapy import Request
+from WebCrawler.items import ReviewLoLItem
 
 
 class ScrapyLolSpider(scrapy.Spider):
-    name = 'scrapy_lol'
-    allowed_domains = ['u.gg']
-    start_urls = ['http://u.gg/']
+  name = 'scrapy_lol'
+  allowed_domains = ['u.gg']
+  start_urls = ['http://u.gg/']
 
-    def parse(self, response):
-        pass
+  def parse_lol(self, response):
+    champions_list = response.css('div.rowgroup div.rt-tr-group')
+
+    # Boucle qui parcours l'ensemble des éléments de la liste des champions
+    for champion in champions_list:
+      item = ReviewLoLItem()
+
+      # Nom du film
+      try:
+        item['title'] = champion.css('h2 a::text')[0].extract()
+      except:
+        item['title'] = 'None'
+
+      yield item
+  
+  def start_requests(self):
+    for url in self.start_url:
+      yield Request(url = url, callback = self.parse_boursorama)
